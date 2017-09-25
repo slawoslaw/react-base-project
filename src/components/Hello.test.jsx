a4dcaeadc69171e7ac1e415c5a60cb0e116b711d
Jarek Andrzejczak <j.andrzejczak@gmail.com>
2017-09-26 00:21:23 +0200

    Add Enzyme for testing

--- src/components/Hello.test.jsx
+++ src/components/Hello.test.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount, shallow } from 'enzyme';
 
 import Hello from './Hello';
 
 describe('Hello', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Hello/>, div);
+    mount(<Hello/>);
+  });
+
+  describe('render', () => {
+    it('should render the Hello', () => {
+      const hello = shallow(<Hello name={'Jarek'}/>);
+      const h1 = <h1>Hello Jarek</h1>;
+
+      expect(hello.contains(h1)).toEqual(true);
+    });
   });
 });
+
