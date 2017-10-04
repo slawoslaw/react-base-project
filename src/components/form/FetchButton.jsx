c9402e46a9611d6598899a40dfb112288ce852e1
Jarek Andrzejczak <j.andrzejczak@gmail.com>
2017-10-05 00:36:37 +0200

    Reusable fetch button

--- src/components/form/FetchButton.jsx
+++ src/components/form/FetchButton.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {FetchData} from '../../services/github';
 
 export default class FetchButton extends React.Component {
   constructor(props) {
@@ -16,20 +15,22 @@ export default class FetchButton extends React.Component {
     if (!this.state.fetched && !this.state.inProgress) {
       this.setState({inProgress: true});
 
-      FetchData(this.props.repo)
+      this.props.fetchFn()
         .then((response) => {
           this.setState({
             fetched: true,
             inProgress: false
           });
-          console.log(response);
+
+          this.props.onFetch(response);
         })
         .catch((error) => {
           this.setState({
             fetched: false,
             inProgress: false
           });
-          console.log(error);
+
+          this.props.onFetch(error);
         });
     }
   }
--- src/services/technologies.js
+++ src/services/technologies.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import {Link} from './github';
+import {Link, FetchData} from './github';
 import FetchButton from '../components/form/FetchButton.jsx';
+
+function onFetch(data) {
+  console.log(data);
+}
 
 export function TechnologiesParser(list) {
   return list.map((item, index) => {
     const link = Link(item.repo);
+    const fetchFn = () => {
+      return FetchData(item.repo);
+    };
 
     return {
       title: item.name,
       subtitle: <a href={link} target="_blank">{item.repo}</a>,
-      content: <FetchButton repo={item.repo} />
+      content: <FetchButton fetchFn={fetchFn} onFetch={onFetch} />
     };
   });
 };
