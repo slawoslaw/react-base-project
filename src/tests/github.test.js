a3299c59a6df9b893946ae86d46d6c6bafaedeb9
Jarek Andrzejczak <j.andrzejczak@gmail.com>
2017-10-06 23:41:39 +0200

    Add some tests

---
+++ src/tests/github.test.js
@@ -0,0 +1,24 @@
+import {Link, ApiLink, FetchData} from '../services/github';
+
+const repo = 'facebook/react';
+
+describe('Github service', () => {
+  describe('Link', () => {
+    it('should render proper github link', () => {
+      expect(Link(repo)).toEqual('https://github.com/facebook/react');
+    });
+  });
+
+  describe('ApiLink', () => {
+    it('should render github api link', () => {
+      expect(ApiLink(repo)).toEqual('https://api.github.com/repos/facebook/react');
+    });
+  });
+
+  describe('FetchData', () => {
+    it('should return promise', () => {
+      expect(FetchData(repo).constructor).toEqual(Promise);
+    });
+  });
+
+});
